82866a09101a5f17f3a3768b1f7ae1091dba6dbe
Debasnana Pattanaik <pattanaikd1998@gmail.com>
2025-11-02 11:33:08 +0530

    delete all files

--- empl.java
+++
@@ -1,5 +0,0 @@
-class Emp{
-  public static void main(String[] args){
-   System.out.println("testing is called");
-  }
-}
--- src/main/java/com/customer/repository/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.customer.repository;
-
-import com.customer.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepo extends JpaRepository<Customer,String> {
-}
